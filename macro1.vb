68a9a2cb0b59aa03e020e3b7af0fe303f34d37e3
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2018-10-19 14:28:38 -0400

    Create macro1.vb

---
+++ macro1.vb
@@ -0,0 +1,39 @@
+Sub InsertSignature()
+'
+' Print Decision Letter Macro
+' This Macro is intended for all Regular admit letters sent out through Xavier. 
+' This will automatically add Aaron Meis signature to each individual letter stored locally on undergraduate instance of slate.
+' 
+' Created by Nick Stone 9/25/17
+' Updated by Jonathan Thortnon 9/26/18
+
+Selection.Find.Execute Replace:=wdReplaceAll
+
+Selection.WholeStory
+Selection.Find.ClearFormatting
+Selection.Find.Replacement.ClearFormatting
+With Selection.Find
+.Text = "Sincerely,"
+.Replacement.Text = "Sincerely,"
+
+Do While .Execute
+With Dialogs(wdDialogInsertPicture)
+.Name = "https://admissions.xavier.edu/www/images/Aaron%20Mies%20SignatureMacro2.png"
+Selection.TypeText Text:="Sincerely," & vbCrLf & vbCrLf
+
+
+
+
+.Execute
+End With
+Loop
+.Forward = True
+.Wrap = wdFindContinue & vcCrLf
+.Format = False
+.MatchCase = False
+.MatchWholeWord = False
+.MatchWildcards = False
+.MatchSoundsLike = False
+.MatchAllWordForms = False
+End With
+End Sub
